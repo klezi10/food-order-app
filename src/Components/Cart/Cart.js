5da59daf7b0701ff2b9e0308003880893a5fb6c0
Kles <klesta.luli@gmail.com>
2022-05-24 11:46:07 +0300

    cart component

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,22 @@
+import classes from "./Cart.module.css"
+
+
+export default function Cart(props) {
+    const cartItems = <ul className={classes['cart-items']}>{[{
+        id: '1', name: 'Sushi', amount: 2, price: '10.99'
+    }].map(item => <li>{item.name}</li>)}</ul>
+
+    return (
+        <div>
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>36.50</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']}>Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+        </div>
+    )
+}
