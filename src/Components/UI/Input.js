72db8e197580cc579cdfde996fe482f01b13a563
Kles <klesta.luli@gmail.com>
2022-05-24 11:19:40 +0300

    making the input component reusable

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -3,8 +3,8 @@ import classes from "./Input.module.css"
 export default function Input(props) {
     return (
         <div className={classes.input}>
-            <label>Amount</label>
-            <input type="number" />
+            <label htmlFor={props.input.id}>{props.label}</label>
+            <input {...props.input} />
         </div>
     )
 }
