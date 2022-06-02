86cf00ace95033948fc13653192271bd8a13e972
Kles <klesta.luli@gmail.com>
2022-06-02 12:40:54 +0400

    output cart items

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import Modal from "../UI/Modal"
 import classes from "./Cart.module.css"
 import CartContext from "../../store/cart-context"
+import CartItem from "./CartItem"
 
 export default function Cart(props) {
     const cartCtx = useContext(CartContext)
@@ -10,8 +11,21 @@ export default function Cart(props) {
 
     const hasItems = cartCtx.items.length > 0
 
+    function addCartItemHandler(item) { }
+
+    function removeCartItemHandler(id) { }
+
     const cartItems = <ul className={classes['cart-items']}>{
-        cartCtx.items.map(item => <li key={item.id}>{item.name}</li>)}</ul>
+        cartCtx.items.map(item => (
+            <CartItem
+                key={item.id}
+                name={item.name}
+                amount={item.amount}
+                price={item.price}
+                onAdd={addCartItemHandler.bind(null, item)}
+                onRemove={removeCartItemHandler.bind(null, item.id)}
+            />
+        ))}</ul>
 
     return (
         <Modal onDismiss={props.onDismiss}>
