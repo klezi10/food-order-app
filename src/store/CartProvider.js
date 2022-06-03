1b29714eb5cd847f2df4196b206f48b382d09f41
Kles <klesta.luli@gmail.com>
2022-06-03 13:20:30 +0400

    updated amounts so an existing item isn't showing multiple times

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,8 +8,29 @@ const defaultCartState = {
 
 function cartReducer(state, action) {
     if (action.type === "ADD") {
-        const updatedItems = state.items.concat(action.item)
+
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        )
+        const existingCartItem = state.items[existingCartItemIndex]
+
+        let updatedItem;
+        let updatedItems;
+
+        if (existingCartItem) {
+            updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            }
+            updatedItems = [...state.items]
+            updatedItems[existingCartItemIndex] = updatedItem
+        } else {
+            updatedItem = {...action.item}
+            updatedItems = state.items.concat(updatedItem)
+        }
+
+      
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
@@ -22,11 +43,11 @@ export default function CartProvider(props) {
     const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
 
     function addItemHandler(item) {
-        dispatchCartAction({type: "ADD", item: item})
-     }
+        dispatchCartAction({ type: "ADD", item: item })
+    }
 
-    function removeItemHandler(id) { 
-        dispatchCartAction({type: "REMOVE", id: id})
+    function removeItemHandler(id) {
+        dispatchCartAction({ type: "REMOVE", id: id })
     }
 
     const cartContext = {
