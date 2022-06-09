02ea82c2dfb75f33d643e22c4953e3ebc9967526
Kles <klesta.luli@gmail.com>
2022-06-09 17:19:35 +0400

    added bump to cart when adding new items (useEffect and useState)

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,22 +1,45 @@
-import { useContext } from "react"
-import classes from "./HeaderCartButton.module.css"
-import CartIcon from "../Cart/CartIcon"
-import CartContext from "../../store/cart-context"
+import { useContext, useEffect, useState } from "react";
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 export default function HeaderCartButton(props) {
-    const cartCtx = useContext(CartContext);
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
-        return currentNumber + item.amount
-    }, 0)
+  const { items } = cartCtx;
 
-    return (
-        <button className={classes.button} onClick={props.onClick}>
-            <span className={classes.icon}>
-                <CartIcon />
-            </span>
-            <span>Your Cart</span>
-            <span className={classes.badge}>{numberOfCartItems}</span>
-        </button>
-    )
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setBtnIsHighlighted(true);
+    
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+        clearTimeout(timer)
+    }
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
 }
